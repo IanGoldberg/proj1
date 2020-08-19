c464a0e8b7e5c4a61c2185d2df1e5372cbb5971b
Ian Goldberg <iangoldberg827@gmail.com>
2020-08-19 00:56:49 -0500

    test commit

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("this is a test")
+from selenium import webdriver
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://google.com")
+print(driver.title)
+driver.quit()
